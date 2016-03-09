bc0ec07e60786cb9a9e0a6c12e2018bfa30f88ed
kkk359641513 <359641513@qq.com>
2016-03-09 11:22:18 +0800

    test

---
+++ kkktest11111/kkktest11111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kkktest11111
+//
+//  Created by kkk on 16/3/9.
+//  Copyright © 2016年 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kkktest11111/kkktest11111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kkktest11111
+//
+//  Created by kkk on 16/3/9.
+//  Copyright © 2016年 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
